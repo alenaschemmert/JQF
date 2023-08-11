af0b9892aa84b97ceacf2ebcb0b0daaa6e08f5a8
kaffeesuchtie <alena.sch95@gmail.com>
2023-08-11 17:50:20 +0200

    workin nextflow afl

--- examples/src/test/java/de/schemmea/ma/NfTest.java
+++ examples/src/test/java/de/schemmea/ma/NfTest.java
@@ -5,85 +5,74 @@ import edu.berkeley.cs.jqf.fuzz.JQF;
 import nextflow.cli.CmdRun;
 import nextflow.cli.Launcher;
 import nextflow.plugin.Plugins;
-import org.junit.After;
-import org.junit.Before;
+
+import org.junit.Assume;
 import org.junit.runner.RunWith;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static de.schemmea.ma.Util.getFileName;
 
 @RunWith(JQF.class)
-class NfTest {
+public class NfTest {
 
 
-    static int iteration = 0;
-
-
-    @Before
-    public void setup() {
-
-    }
-
-    static File currentFile = null;
-
-    @Fuzz
-    public void testWorkflow(File inputFile) {
-        System.out.println("ITERATION " + ++iteration + "\n");
-        currentFile = inputFile;
-
-        String filename = inputFile.getAbsolutePath();
-        String[] orig_args2 = new String[]{"run", filename};
-        List<String> args2 = List.of(filename);
-
-        Launcher launcher = new Launcher().command(orig_args2);//.run();
-
-        CmdRun myRunner = new CmdRun();
-        myRunner.setArgs(args2);
-        myRunner.setLauncher(launcher);
-
-        myRunner.run();
+    private void serializeInputStream(InputStream in, String filename) throws IOException {
+        Path path = Paths.get(filename);
+        try (BufferedWriter out = Files.newBufferedWriter(path)) {
+            int b;
+            while ((b = in.read()) != -1) {
+                out.write(b);
+            }
+        }
     }
 
     @Fuzz
-    public void testAFL(InputStream inputStream) {
-        /**
+    public void testAFL(InputStream inputStream) throws IOException {
+        /*
          * install afl
          * https://medium.com/@ayushpriya10/fuzzing-applications-with-american-fuzzy-lop-afl-54facc65d102
+         * https://www.dannyvanheumen.nl/post/java-fuzzing-with-afl-and-jqf/
          */
         String filename = getFileName();
-        File file = new File(filename);
+        try {
+            serializeInputStream(inputStream, filename);
 
-        byte[] buffer = new byte[1024];
+            List<String> args2 = List.of(filename);
+            String[] orig_args2 = new String[]{"run", filename};
 
-        try (OutputStream outputStream = new FileOutputStream(file, false)) {
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-        } catch (Exception e) {
-            /**ignore**/
+            Launcher launcher = new Launcher().command(orig_args2);//.run();
+
+            CmdRun myRunner = new CmdRun();
+            myRunner.setArgs(args2);
+            myRunner.setLauncher(launcher);
+
+            myRunner.run();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Throwable t) {
+            Assume.assumeNoException(t);
+        } finally {
+
+            //instead of @After
+            Plugins.stop();
+            Files.delete(Paths.get(filename));
+            //nextflow clean -f does not work?!
+            //  int status = new Launcher().command(new String[]{"clean", "-f"}).run();
         }
-
-        testWorkflow(file);
     }
 
 
-    @After
-    public void cleanUp() {
-        //plugins won't stop after sriptcompilation exception
-        Plugins.stop();
+    @Fuzz
+    public void testTest() throws IOException {
 
-        if (currentFile != null) {
-            currentFile.delete();
-        }
+        String filename = "/home/alena/source/JQF/examples/target/seeds/nextflow/hello.nf";
 
-        //nextflow clean -f
-        int status = new Launcher().command(new String[]{"clean", "-f"}).run();
+        testAFL(Files.newInputStream(Paths.get(filename)));
     }
 
 }
